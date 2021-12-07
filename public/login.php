84589a31371cc16ab1797e6c31a2c75c0b6eefc9
Anandhu M S <anandhumeleveettil@gmail.com>
2021-12-07 22:04:23 +0530

    login

---
+++ public/login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="./stylesheet/login.css">
+</head>
+<body>
+    <div id="content-box">
+        <div class="img-con">
+            <h1>oSHOP</h1>
+            <div class="content">
+                <form class="forms" action="">
+                    <input type="text" name="userid" value="" placeholder="username"><br><br>
+                    <input type="password" name="passwd" value="" placeholder="password"><br><br>
+                    <input type="submit" value="submit">
+                </form>                         
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ public/signup.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign up</title>
+    <link rel="stylesheet" href="./stylesheet/login.css">
+</head>
+<body>
+    <div id="content-box">
+        <div class="img-con">
+            <h1>oSHOP</h1>
+            <div class="content">
+                <form action="">
+                    <input type="text" name="fname" value="" placeholder="FIRSTNAME"><br><br>
+                    <input type="text" name="lname" value="" placeholder="LASTNAME"><br><br>
+                    <input type="email" name="email" value="" placeholder="EMAIL"><br><br>
+                    <input type="password" name="pswd" value="" placeholder="PASSWORD"><br><br>
+                    <input type="password" name="cpswd" value="" placeholder="CONFIRM PASSWORD">
+                    <br><br>
+                    <input type="submit" value="Submit">
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
